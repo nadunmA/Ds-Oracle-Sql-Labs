e9b84e0b27840888a88f188fa9cb344a8676d534
Nadun Algoda <168359459+nadunmA@users.noreply.github.com>
2026-03-03 12:42:14 +0530

    Update to tute5.sql

--- tute5.sql
+++ tute5.sql
@@ -1,6 +1,3 @@
--- IT23192850
--- Nadun M.A
--- 3Y S1 WE 4.2
 -- practical 5
 
 SET SERVEROUTPUT ON;
@@ -188,4 +185,5 @@ END;
 SELECT clno, company, pdate, qty
 FROM purchase
 ORDER BY pdate;
+
 ROLLBACK;
